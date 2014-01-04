75f566ce7dc5e8e6f6313dcd9adcdbfa8fafb48b
Andrew Peck <anp@ucla.edu>
2014-01-03 16:53:03 -0800

    preprocessor conditionals

--- analysis/unpacker/unpack.cpp
+++ analysis/unpacker/unpack.cpp
@@ -23,13 +23,14 @@ using namespace std;
 
 double fraction=0.5;  //fractional threshold
 double Vthresh=-50;  //fixed threshold
-const int nchn=4; //change nchn for the number of channels
 
 void DecodeWaveform(unsigned short WaveIn[], Double_t WaveOut[], double &Vpeak, int &PeakBin);
 void InterpolateWaveform(Double_t ArrA[], Double_t ArrB[]);
 void IntegrateCharge(Double_t chn[], int PeakBin, double& Qint);
 void PulseTime(Double_t chn[], Double_t t[], double& CTHtime, double& FTHtime, double& CTHWidth, double& FTHWidth, double Vpeak, int PeakBin);
 void MakePlots(char* filename);
+
+#define NCHN 2
 
 struct Header_t {
     char           event_header[4];
@@ -46,14 +47,22 @@ struct Header_t {
 };
 
 struct Waveform_t {
-        char           chn0_header[4];
-        unsigned short chn0[1024];
-        char           chn1_header[4];
-        unsigned short chn1[1024];
-        char           chn2_header[4];
-        unsigned short chn2[1024];
-        char           chn3_header[4]; 
-        unsigned short chn3[1024]; 
+#if NCHN>0
+    char           chn0_header[4];
+    unsigned short chn0[1024];
+#endif
+#if NCHN>1
+    char           chn1_header[4];
+    unsigned short chn1[1024];
+#endif
+#if NCHN>2
+    char           chn2_header[4];
+    unsigned short chn2[1024];
+#endif
+#if NCHN>3
+    char           chn3_header[4]; 
+    unsigned short chn3[1024]; 
+#endif
 };
 
 int unpack(char *filename) {
@@ -62,31 +71,42 @@ int unpack(char *filename) {
     gStyle->SetTitleBorderSize(0);
     gStyle->SetPalette(1);
 
-    
+
     Header_t header;
     Waveform_t waveform;
-    
+
 
     Double_t t[1024];
+#if NCHN>0
     Double_t chn0[1024];
+#endif
+
+#if NCHN>1
     Double_t chn1[1024];
+#endif
+
+#if NCHN>2
     Double_t chn2[1024];
+#endif
+
+#if NCHN>3
     Double_t chn3[1024];
+#endif
 
     unsigned int eventNum; 
 
-    double Qint[nchn];
-    double Vpeak[nchn];
+    double Qint[NCHN];
+    double Vpeak[NCHN];
 
-    double CTHtime[nchn];
-    double CTHWidth[nchn];
+    double CTHtime[NCHN];
+    double CTHWidth[NCHN];
     double CTHFlightTime;
 
-    double FTHtime[nchn];
-    double FTHWidth[nchn];
+    double FTHtime[NCHN];
+    double FTHWidth[NCHN];
     double FTHFlightTime;
 
-    int PeakBin[nchn];
+    int PeakBin[NCHN];
     std::time_t EventDate;
 
     // open the binary waveform file
@@ -110,44 +130,52 @@ int unpack(char *filename) {
     rec->Branch("EventDate", &EventDate ,"EventDate");
 
     //channel 0 branches
-        rec->Branch("chn0", &chn0, "chn0[1024]/D");
-        rec->Branch("Qint0", &Qint[0] ,"Qint0/D");
-        rec->Branch("Vpeak0", &Vpeak[0],	"Vpeak0/D");
-        rec->Branch("PeakBin0", &PeakBin[0],	"PeakBin0/I");
-        rec->Branch("CTHtime0", &CTHtime[0],	"CTHtime0/D");
-        rec->Branch("FTHtime0", &FTHtime[0],	"FTHtime0/D");
-        rec->Branch("CTHWidth0", &CTHWidth[0],	"CTHWidth0/D");
-        rec->Branch("FTHWidth0", &FTHWidth[0],	"FTHWidth0/D");
+#if NCHN > 0
+    rec->Branch("chn0", &chn0, "chn0[1024]/D");
+    rec->Branch("Qint0", &Qint[0] ,"Qint0/D");
+    rec->Branch("Vpeak0", &Vpeak[0],	"Vpeak0/D");
+    rec->Branch("PeakBin0", &PeakBin[0],	"PeakBin0/I");
+    rec->Branch("CTHtime0", &CTHtime[0],	"CTHtime0/D");
+    rec->Branch("FTHtime0", &FTHtime[0],	"FTHtime0/D");
+    rec->Branch("CTHWidth0", &CTHWidth[0],	"CTHWidth0/D");
+    rec->Branch("FTHWidth0", &FTHWidth[0],	"FTHWidth0/D");
+#endif
 
     //channel 1 branches
-        rec->Branch("chn1", &chn1,"chn1[1024]/D");
-        rec->Branch("Qint1", &Qint[1] ,"Qint1/D");
-        rec->Branch("Vpeak1", &Vpeak[1],	"Vpeak1/D");
-        rec->Branch("PeakBin1", &PeakBin[1],	"PeakBin1/I");
-        rec->Branch("CTHtime1", &CTHtime[1],	"CTHtime1/D");
-        rec->Branch("FTHtime1", &FTHtime[1],	"FTHtime1/D");
-        rec->Branch("CTHWidth1", &CTHWidth[1],	"CTHWidth1/D");
-        rec->Branch("FTHWidth1", &FTHWidth[1],	"FTHWidth1/D");
+#if NCHN > 1
+    rec->Branch("chn1", &chn1,"chn1[1024]/D");
+    rec->Branch("Qint1", &Qint[1] ,"Qint1/D");
+    rec->Branch("Vpeak1", &Vpeak[1],	"Vpeak1/D");
+    rec->Branch("PeakBin1", &PeakBin[1],	"PeakBin1/I");
+    rec->Branch("CTHtime1", &CTHtime[1],	"CTHtime1/D");
+    rec->Branch("FTHtime1", &FTHtime[1],	"FTHtime1/D");
+    rec->Branch("CTHWidth1", &CTHWidth[1],	"CTHWidth1/D");
+    rec->Branch("FTHWidth1", &FTHWidth[1],	"FTHWidth1/D");
+#endif
 
     //channel 2 branches
-        rec->Branch("chn2", &chn2,              "chn2[1024]/D");
-        rec->Branch("Qint2", &Qint[2],          "Qint2/D");
-        rec->Branch("Vpeak2", &Vpeak[2],	    "Vpeak2/D");
-        rec->Branch("PeakBin2", &PeakBin[2],	"PeakBin2/I");
-        rec->Branch("CTHtime2", &CTHtime[2],	"CTHtime2/D");
-        rec->Branch("FTHtime2", &FTHtime[2],	"FTHtime2/D");
-        rec->Branch("CTHWidth2", &CTHWidth[2],	"CTHWidth2/D");
-        rec->Branch("FTHWidth2", &FTHWidth[2],	"FTHWidth2/D");
+#if NCHN > 2
+    rec->Branch("chn2", &chn2,              "chn2[1024]/D");
+    rec->Branch("Qint2", &Qint[2],          "Qint2/D");
+    rec->Branch("Vpeak2", &Vpeak[2],	    "Vpeak2/D");
+    rec->Branch("PeakBin2", &PeakBin[2],	"PeakBin2/I");
+    rec->Branch("CTHtime2", &CTHtime[2],	"CTHtime2/D");
+    rec->Branch("FTHtime2", &FTHtime[2],	"FTHtime2/D");
+    rec->Branch("CTHWidth2", &CTHWidth[2],	"CTHWidth2/D");
+    rec->Branch("FTHWidth2", &FTHWidth[2],	"FTHWidth2/D");
+#endif
 
     //channel 3 branches
-        rec->Branch("chn3", &chn3,              "chn3[1024]/D");
-        rec->Branch("Qint3", &Qint[3],          "Qint3/D");
-        rec->Branch("Vpeak3", &Vpeak[3],	    "Vpeak3/D");
-        rec->Branch("PeakBin3", &PeakBin[3],	"PeakBin3/I");
-        rec->Branch("CTHtime3", &CTHtime[3],	"CTHtime3/D");
-        rec->Branch("FTHtime3", &FTHtime[3],	"FTHtime3/D");
-        rec->Branch("CTHWidth3", &CTHWidth[3],	"CTHWidth3/D");
-        rec->Branch("FTHWidth3", &FTHWidth[3],	"FTHWidth3/D");
+#if NCHN > 3
+    rec->Branch("chn3", &chn3,              "chn3[1024]/D");
+    rec->Branch("Qint3", &Qint[3],          "Qint3/D");
+    rec->Branch("Vpeak3", &Vpeak[3],	    "Vpeak3/D");
+    rec->Branch("PeakBin3", &PeakBin[3],	"PeakBin3/I");
+    rec->Branch("CTHtime3", &CTHtime[3],	"CTHtime3/D");
+    rec->Branch("FTHtime3", &FTHtime[3],	"FTHtime3/D");
+    rec->Branch("CTHWidth3", &CTHWidth[3],	"CTHWidth3/D");
+    rec->Branch("FTHWidth3", &FTHWidth[3],	"FTHWidth3/D");
+#endif
 
 
     //flight time branches
@@ -186,28 +214,62 @@ int unpack(char *filename) {
 
         // Read in Waveform Data
         fread(&waveform, sizeof(waveform), 1, f);
-        
+
         // decode amplitudes in mV and find Peak Voltage
+#if NCHN > 0
         DecodeWaveform(waveform.chn0, chn0, Vpeak[0], PeakBin[0]);
+#endif
+
+#if NCHN > 1
         DecodeWaveform(waveform.chn1, chn1, Vpeak[1], PeakBin[1]);
+#endif
+
+#if NCHN > 2
         DecodeWaveform(waveform.chn2, chn2, Vpeak[2], PeakBin[2]);
+#endif
+
+#if NCHN > 3
         DecodeWaveform(waveform.chn3, chn3, Vpeak[3], PeakBin[3]);
+#endif
 
         //if Vthresh undefined, define as 1/2 avg of first two pulses
         if (Vthresh==0)
             Vthresh=0.25*(Vpeak[0]+Vpeak[1]);
 
         //Measure PulseTime (Fractional and Constant Threshold Time)
+#if NCHN > 0
         PulseTime(chn0, t, CTHtime[0], FTHtime[0], CTHWidth[0], FTHWidth[0], Vpeak[0], PeakBin[0]);
+#endif
+
+#if NCHN > 1
         PulseTime(chn1, t, CTHtime[1], FTHtime[1], CTHWidth[1], FTHWidth[1], Vpeak[1], PeakBin[1]);
+#endif
+
+#if NCHN > 2
         PulseTime(chn2, t, CTHtime[2], FTHtime[2], CTHWidth[2], FTHWidth[2], Vpeak[2], PeakBin[2]);
+#endif
+
+#if NCHN > 3
         PulseTime(chn3, t, CTHtime[3], FTHtime[3], CTHWidth[3], FTHWidth[3], Vpeak[3], PeakBin[3]);
+#endif
 
         //Integrate Charge, Find Pedestal
+#if NCHN > 0
         IntegrateCharge(chn0, PeakBin[0], Qint[0]);
+#endif 
+
+#if NCHN > 1
         IntegrateCharge(chn1, PeakBin[1], Qint[1]);
+#endif 
+
+#if NCHN > 2
         IntegrateCharge(chn2, PeakBin[2], Qint[2]);
+#endif 
+
+#if NCHN > 3
         IntegrateCharge(chn3, PeakBin[3], Qint[3]);
+#endif 
+
 
         //Calculate time-of-flight
         CTHFlightTime=(CTHtime[1]-CTHtime[0]);
@@ -224,7 +286,7 @@ int unpack(char *filename) {
 
     //Write Tree
     rec->Write();
-    
+
     //Close ROOTfile
     outfile->Close();
 
@@ -294,7 +356,7 @@ void PulseTime(Double_t chn[], Double_t t[], double& CTHtime, double& FTHtime, d
     for (Int_t i=20; i<2028; i++)  //discard first and last 10 time bins
     {
         if ((i>2*(PeakBin-30)) && (i<2*(PeakBin+30))) {
-            
+
             //Find Constant Threshold Cross Time 
             if ( (cth_rise==false) && (chnInterp[i] < Vthresh) ) {
                 CTHtime=t[i/2]+t[i%2]/2;
@@ -303,11 +365,11 @@ void PulseTime(Double_t chn[], Double_t t[], double& CTHtime, double& FTHtime, d
 
             //CTH Pulse Width
             if ((cth_rise==true) && (cth_fall==false) \
-                 && (chnInterp[i]>(Vthresh) )) {
+                    && (chnInterp[i]>(Vthresh) )) {
                 CTHWidth=t[i/2]+t[i%2]/2-CTHtime;
                 cth_fall=true;
             }
-            
+
             //Find Fractional Threshold Cross Time
             if ((fth_rise==false)&&(chnInterp[i]<(Vpeak*fraction))) {
                 FTHtime=t[i/2]+t[i%2]/2; 
@@ -316,7 +378,7 @@ void PulseTime(Double_t chn[], Double_t t[], double& CTHtime, double& FTHtime, d
 
             //FTH Pulse width
             if ((fth_rise==true) && (fth_fall==false) \
-                && (chnInterp[i]>(Vpeak*fraction))) {
+                    && (chnInterp[i]>(Vpeak*fraction))) {
                 FTHWidth=t[i/2]+t[i%2]/2-FTHtime;
                 fth_fall=true;
             }
@@ -328,7 +390,7 @@ void MakePlots(char* filename) {
     //open the root file
     TFile *rootfile = new TFile(Form("%s.root", filename),"UPDATE");
     TTree *rec = (TTree *)rootfile->Get("rec");
-    
+
 
     //TH1F *h1 = new TH1F("h1","Fractional Threshold Flighttime (Vpeak<-10)",43,-2,2);
     //TH1F *h1 = new TH1F("h1","Fractional Threshold Flighttime (Vpeak<-10)",123,-6,6);
@@ -350,45 +412,53 @@ void MakePlots(char* filename) {
     sinefit->SetParameter(1, 0.15);
     sinefit->SetParameter(2, -8.2);
 
-    TH2F *ch0 = new TH2F("ch0","channel0 trace",225,0,225,620,-310,310);
+#if NCHN>0
+    TH2F *ch0 = new TH2F("ch0","channel0 trace",225,0,225,520,-500,20);
     ch0->GetXaxis()->SetTitle("time (ns)");
     ch0->GetYaxis()->SetTitle("Voltage (mv)");
     rec->Draw("chn0:t>>ch0","(Vpeak0<-10)");
-    ch0->Fit("sinefit");
+    //ch0->Fit("sinefit");
+    ch0->Write();
+#endif
 
-    TH2F *ch1 = new TH2F("ch1","channel1 trace",225,0,225,620,-310,310);
+#if NCHN>1
+    TH2F *ch1 = new TH2F("ch1","channel1 trace",225,0,225,520,-500,20);
     ch1->GetXaxis()->SetTitle("time (ns)");
     ch1->GetYaxis()->SetTitle("Voltage (mv)");
     rec->Draw("chn1:t>>ch1","(Vpeak1<-10)");
-    ch1->Fit("sinefit");
+    //ch1->Fit("sinefit");
+    ch1->Write();
+#endif
 
+#if NCHN>2
     TH2F *ch2 = new TH2F("ch2","channel2 trace",225,0,225,620,-310,310);
     ch2->GetXaxis()->SetTitle("time (ns)");
     ch2->GetYaxis()->SetTitle("Voltage (mv)");
     rec->Draw("chn2:t>>ch2","(Vpeak2<-10)");
     ch2->Fit("sinefit");
+    ch2->Write();
+#endif
 
+#if NCHN>3
     TH2F *ch3 = new TH2F("ch3","channel3 trace",225,0,225,620,-310,310);
     ch3->GetXaxis()->SetTitle("time (ns)");
     ch3->GetYaxis()->SetTitle("Voltage (mv)");
     rec->Draw("chn3:t>>ch3","(Vpeak3<-10)");
     ch3->Fit("sinefit");
+    ch3->Write();
+#endif
 
     TProfile *hprof = new TProfile("hprof", "Flight time dependence on peak voltage",100,0,400,0,100);
     hprof->GetXaxis()->SetTitle("Peak Voltage (-mV)");
     hprof->GetYaxis()->SetTitle("abs(Flight Time) (ns)");
     rec->Draw("abs(FTHFlightTime):abs(0.5*(Vpeak1+Vpeak0))>>hprof",
-              "(Vpeak0<0 && Vpeak1<0)");
+            "(Vpeak0<0 && Vpeak1<0)");
 
 
     h1->Write();
     h2->Write();
     h3->Write();
-    ch0->Write();
-    ch1->Write();
-    ch2->Write();
-    ch3->Write();
     hprof->Write();
-    
+
     rootfile->Close();
 }
