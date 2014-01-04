c23e4338b51beae6d19cb0c8c2e7ad539289bd0d
Andrew Peck <anp@ucla.edu>
2014-01-03 16:16:07 -0800

    Four input mode

--- analysis/unpacker/unpack.cpp
+++ analysis/unpacker/unpack.cpp
@@ -1,6 +1,5 @@
 //System Headers
 #include <string.h>
-#include <TH2.h>
 #include <stdio.h>
 #include <TString.h>
 #include <iostream>
@@ -13,6 +12,8 @@
 #include <TFile.h>
 #include <TRandom3.h>
 #include <TH1.h>
+#include <TH2.h>
+#include <TF1.h>
 #include <TCanvas.h>
 #include <TStyle.h>
 #include <TProfile.h> 
@@ -21,7 +22,8 @@
 using namespace std; 
 
 double fraction=0.5;  //fractional threshold
-double Vthresh=0;  //fixed threshold
+double Vthresh=-50;  //fixed threshold
+const int nchn=4; //change nchn for the number of channels
 
 void DecodeWaveform(unsigned short WaveIn[], Double_t WaveOut[], double &Vpeak, int &PeakBin);
 void InterpolateWaveform(Double_t ArrA[], Double_t ArrB[]);
@@ -44,33 +46,35 @@ struct Header_t {
 };
 
 struct Waveform_t {
-    char           chn0_header[4];
-    unsigned short chn0[1024];
-    char           chn1_header[4];
-    unsigned short chn1[1024];
-    //   char           chn3_header[4]; //uncomment for channel 2
-    //   unsigned short chn3[1024]; //uncomment for channel 2 
-    //   char           chn4_header[4]; //uncomment for channel 3 
-    //   unsigned short chn4[1024]; //uncomment for channel 3 
+        char           chn0_header[4];
+        unsigned short chn0[1024];
+        char           chn1_header[4];
+        unsigned short chn1[1024];
+        char           chn2_header[4];
+        unsigned short chn2[1024];
+        char           chn3_header[4]; 
+        unsigned short chn3[1024]; 
 };
 
-void unpack(char *filename) {
+int unpack(char *filename) {
+
     gROOT->SetStyle("Plain");
     gStyle->SetTitleBorderSize(0);
     gStyle->SetPalette(1);
 
+    
     Header_t header;
     Waveform_t waveform;
+    
+
     Double_t t[1024];
     Double_t chn0[1024];
     Double_t chn1[1024];
-    //Double_t chn2[1024]; //uncomment for channel 2 
-    //Double_t chn3[1024]; //uncomment for channel 3
-    Int_t n;
+    Double_t chn2[1024];
+    Double_t chn3[1024];
+
     unsigned int eventNum; 
 
-    //change nchn for the number of channels
-    int nchn=2;
     double Qint[nchn];
     double Vpeak[nchn];
 
@@ -86,9 +90,15 @@ void unpack(char *filename) {
     std::time_t EventDate;
 
     // open the binary waveform file
-    FILE *f = fopen(Form("%s.dat", filename), "r");
+    FILE *f; 
+    if ( (f=fopen(Form("%s.dat",filename),"r")) == NULL) {
+        cout << "File not found\n";
+        return 1; 
+    }
+    else {
+        cout << "File opened\n";
+    }
 
-    //open the root file
     TFile *outfile = new TFile(Form("%s.root", filename), "RECREATE");
 
     // plant tree
@@ -100,24 +110,44 @@ void unpack(char *filename) {
     rec->Branch("EventDate", &EventDate ,"EventDate");
 
     //channel 0 branches
-    rec->Branch("chn0", &chn0, "chn0[1024]/D");
-    rec->Branch("Qint0", &Qint[0] ,"Qint0/D");
-    rec->Branch("Vpeak0", &Vpeak[0],	"Vpeak0/D");
-    rec->Branch("PeakBin0", &PeakBin[0],	"PeakBin0/I");
-    rec->Branch("CTHtime0", &CTHtime[0],	"CTHtime0/D");
-    rec->Branch("FTHtime0", &FTHtime[0],	"FTHtime0/D");
-    rec->Branch("CTHWidth0", &CTHWidth[0],	"CTHWidth0/D");
-    rec->Branch("FTHWidth0", &FTHWidth[0],	"FTHWidth0/D");
+        rec->Branch("chn0", &chn0, "chn0[1024]/D");
+        rec->Branch("Qint0", &Qint[0] ,"Qint0/D");
+        rec->Branch("Vpeak0", &Vpeak[0],	"Vpeak0/D");
+        rec->Branch("PeakBin0", &PeakBin[0],	"PeakBin0/I");
+        rec->Branch("CTHtime0", &CTHtime[0],	"CTHtime0/D");
+        rec->Branch("FTHtime0", &FTHtime[0],	"FTHtime0/D");
+        rec->Branch("CTHWidth0", &CTHWidth[0],	"CTHWidth0/D");
+        rec->Branch("FTHWidth0", &FTHWidth[0],	"FTHWidth0/D");
 
     //channel 1 branches
-    rec->Branch("chn1", &chn1,"chn1[1024]/D");
-    rec->Branch("Qint1", &Qint[1] ,"Qint1/D");
-    rec->Branch("Vpeak1", &Vpeak[1],	"Vpeak1/D");
-    rec->Branch("PeakBin1", &PeakBin[1],	"PeakBin1/I");
-    rec->Branch("CTHtime1", &CTHtime[1],	"CTHtime1/D");
-    rec->Branch("FTHtime1", &FTHtime[1],	"FTHtime1/D");
-    rec->Branch("CTHWidth1", &CTHWidth[1],	"CTHWidth1/D");
-    rec->Branch("FTHWidth1", &FTHWidth[1],	"FTHWidth1/D");
+        rec->Branch("chn1", &chn1,"chn1[1024]/D");
+        rec->Branch("Qint1", &Qint[1] ,"Qint1/D");
+        rec->Branch("Vpeak1", &Vpeak[1],	"Vpeak1/D");
+        rec->Branch("PeakBin1", &PeakBin[1],	"PeakBin1/I");
+        rec->Branch("CTHtime1", &CTHtime[1],	"CTHtime1/D");
+        rec->Branch("FTHtime1", &FTHtime[1],	"FTHtime1/D");
+        rec->Branch("CTHWidth1", &CTHWidth[1],	"CTHWidth1/D");
+        rec->Branch("FTHWidth1", &FTHWidth[1],	"FTHWidth1/D");
+
+    //channel 2 branches
+        rec->Branch("chn2", &chn2,              "chn2[1024]/D");
+        rec->Branch("Qint2", &Qint[2],          "Qint2/D");
+        rec->Branch("Vpeak2", &Vpeak[2],	    "Vpeak2/D");
+        rec->Branch("PeakBin2", &PeakBin[2],	"PeakBin2/I");
+        rec->Branch("CTHtime2", &CTHtime[2],	"CTHtime2/D");
+        rec->Branch("FTHtime2", &FTHtime[2],	"FTHtime2/D");
+        rec->Branch("CTHWidth2", &CTHWidth[2],	"CTHWidth2/D");
+        rec->Branch("FTHWidth2", &FTHWidth[2],	"FTHWidth2/D");
+
+    //channel 3 branches
+        rec->Branch("chn3", &chn3,              "chn3[1024]/D");
+        rec->Branch("Qint3", &Qint[3],          "Qint3/D");
+        rec->Branch("Vpeak3", &Vpeak[3],	    "Vpeak3/D");
+        rec->Branch("PeakBin3", &PeakBin[3],	"PeakBin3/I");
+        rec->Branch("CTHtime3", &CTHtime[3],	"CTHtime3/D");
+        rec->Branch("FTHtime3", &FTHtime[3],	"FTHtime3/D");
+        rec->Branch("CTHWidth3", &CTHWidth[3],	"CTHWidth3/D");
+        rec->Branch("FTHWidth3", &FTHWidth[3],	"FTHWidth3/D");
 
 
     //flight time branches
@@ -125,7 +155,8 @@ void unpack(char *filename) {
     rec->Branch("FTHFlightTime", &FTHFlightTime,	"FTHFlightTime/D");
 
     // loop over all events in data file
-    for (n=0 ; fread(&header, sizeof(header), 1, f) > 0; n++) {
+    Int_t n; 
+    for (n=0; fread(&header, sizeof(header), 1, f) > 0; n++) {
 
         //extract event number
         eventNum=header.serial_number;
@@ -159,6 +190,8 @@ void unpack(char *filename) {
         // decode amplitudes in mV and find Peak Voltage
         DecodeWaveform(waveform.chn0, chn0, Vpeak[0], PeakBin[0]);
         DecodeWaveform(waveform.chn1, chn1, Vpeak[1], PeakBin[1]);
+        DecodeWaveform(waveform.chn2, chn2, Vpeak[2], PeakBin[2]);
+        DecodeWaveform(waveform.chn3, chn3, Vpeak[3], PeakBin[3]);
 
         //if Vthresh undefined, define as 1/2 avg of first two pulses
         if (Vthresh==0)
@@ -167,10 +200,14 @@ void unpack(char *filename) {
         //Measure PulseTime (Fractional and Constant Threshold Time)
         PulseTime(chn0, t, CTHtime[0], FTHtime[0], CTHWidth[0], FTHWidth[0], Vpeak[0], PeakBin[0]);
         PulseTime(chn1, t, CTHtime[1], FTHtime[1], CTHWidth[1], FTHWidth[1], Vpeak[1], PeakBin[1]);
+        PulseTime(chn2, t, CTHtime[2], FTHtime[2], CTHWidth[2], FTHWidth[2], Vpeak[2], PeakBin[2]);
+        PulseTime(chn3, t, CTHtime[3], FTHtime[3], CTHWidth[3], FTHWidth[3], Vpeak[3], PeakBin[3]);
 
         //Integrate Charge, Find Pedestal
         IntegrateCharge(chn0, PeakBin[0], Qint[0]);
         IntegrateCharge(chn1, PeakBin[1], Qint[1]);
+        IntegrateCharge(chn2, PeakBin[2], Qint[2]);
+        IntegrateCharge(chn3, PeakBin[3], Qint[3]);
 
         //Calculate time-of-flight
         CTHFlightTime=(CTHtime[1]-CTHtime[0]);
@@ -193,6 +230,7 @@ void unpack(char *filename) {
 
     //make some extra plots
     MakePlots(filename);
+    return 0;
 }
 
 void DecodeWaveform(unsigned short WaveIn[], Double_t WaveOut[], double &Vpeak, int &PeakBin) {
@@ -307,15 +345,34 @@ void MakePlots(char* filename) {
     h3->GetXaxis()->SetTitle("time (ns)");
     rec->Draw("FTHFlightTime>>h3","(Vpeak0<-100 && Vpeak1<-100)");
 
-    TH2F *h4 = new TH2F("h4","channel0 trace",225,0,225,620,-600,20);
-    h4->GetXaxis()->SetTitle("time (ns)");
-    h4->GetYaxis()->SetTitle("Voltage (mv)");
-    rec->Draw("chn0:t>>h4","(Vpeak0<0 && Vpeak1<0)");
+    TF1 *sinefit = new TF1("sinefit","[0]*sin([1]*x+[2])", 0,200);
+    sinefit->SetParameter(0, 80);
+    sinefit->SetParameter(1, 0.15);
+    sinefit->SetParameter(2, -8.2);
 
-    TH2F *h5 = new TH2F("h5","channel1 trace",225,0,225,620,-600,20);
-    h5->GetXaxis()->SetTitle("time (ns)");
-    h5->GetYaxis()->SetTitle("Voltage (mv)");
-    rec->Draw("chn1:t>>h5","(Vpeak0<0 && Vpeak1<0)");
+    TH2F *ch0 = new TH2F("ch0","channel0 trace",225,0,225,620,-310,310);
+    ch0->GetXaxis()->SetTitle("time (ns)");
+    ch0->GetYaxis()->SetTitle("Voltage (mv)");
+    rec->Draw("chn0:t>>ch0","(Vpeak0<-10)");
+    ch0->Fit("sinefit");
+
+    TH2F *ch1 = new TH2F("ch1","channel1 trace",225,0,225,620,-310,310);
+    ch1->GetXaxis()->SetTitle("time (ns)");
+    ch1->GetYaxis()->SetTitle("Voltage (mv)");
+    rec->Draw("chn1:t>>ch1","(Vpeak1<-10)");
+    ch1->Fit("sinefit");
+
+    TH2F *ch2 = new TH2F("ch2","channel2 trace",225,0,225,620,-310,310);
+    ch2->GetXaxis()->SetTitle("time (ns)");
+    ch2->GetYaxis()->SetTitle("Voltage (mv)");
+    rec->Draw("chn2:t>>ch2","(Vpeak2<-10)");
+    ch2->Fit("sinefit");
+
+    TH2F *ch3 = new TH2F("ch3","channel3 trace",225,0,225,620,-310,310);
+    ch3->GetXaxis()->SetTitle("time (ns)");
+    ch3->GetYaxis()->SetTitle("Voltage (mv)");
+    rec->Draw("chn3:t>>ch3","(Vpeak3<-10)");
+    ch3->Fit("sinefit");
 
     TProfile *hprof = new TProfile("hprof", "Flight time dependence on peak voltage",100,0,400,0,100);
     hprof->GetXaxis()->SetTitle("Peak Voltage (-mV)");
@@ -327,8 +384,10 @@ void MakePlots(char* filename) {
     h1->Write();
     h2->Write();
     h3->Write();
-    h4->Write();
-    h5->Write();
+    ch0->Write();
+    ch1->Write();
+    ch2->Write();
+    ch3->Write();
     hprof->Write();
     
     rootfile->Close();
