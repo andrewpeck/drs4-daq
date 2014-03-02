f106e091f25b51e04dbac7331e864fda82dceb4f
Andrew Peck <anp@ucla.edu>
2014-03-02 18:54:25 +0100

    Cleanup, more comments..

--- drsdaq/src/drsdaq.cpp
+++ drsdaq/src/drsdaq.cpp
@@ -1,22 +1,29 @@
+//------------------------------------------------------------------------------
+// System Headers
+//------------------------------------------------------------------------------
+#include <vector>
+#include <iostream>
 #include <math.h>
 #include <fstream>
 #include <unistd.h>
 #include <fcntl.h>
-#define O_BINARY 0
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/ioctl.h>
 #include <errno.h>
-#define DIR_SEPARATOR '/'
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+//------------------------------------------------------------------------------
+// Local Headers
+//------------------------------------------------------------------------------
 #include "strlcpy.h"
 #include "DRS.h"
 #include "rapidxml.hpp"
-#include <vector>
-#include <iostream>
 #include "progressbar.h"
+#define O_BINARY 0
+#define DIR_SEPARATOR '/'
 
 using namespace rapidxml;
 using namespace std;
@@ -34,36 +41,38 @@ typedef struct {
 void GetTimeStamp(TIMESTAMP &ts);
 void ParseOptions(void);
 
-unsigned char buffer[100000];
+//------------------------------------------------------------------------------
+// Function Prototypes
+//------------------------------------------------------------------------------
 int SaveWaveforms(int fd);
-int calibrate();
 int init();
 
+//------------------------------------------------------------------------------
+// Vars
+//------------------------------------------------------------------------------
+
+unsigned char buffer[100000];
+
 DRS *drs;
-DRSBoard *b;
+DRSBoard *board;
 TIMESTAMP evTimestamp;
 
-int evSerial=0; //event number
-float time_array[1024]; //timebinarray
-float waveform[8][1024]; //8x1024 array (8 channels x 1024 samples)
-int numEvents=1000;
-int waveDepth=1024;
-double center=0.0; //zero point
-double triglevel=-0.02; //trigger level (in VOLTS)
-bool chnOn[4]={};
-int trigsource=0;
-bool posneg=false; //1=rising edge 0=falling edge
-double freq=5; //sampling frequency
+int     evSerial=0;         //event number
+float   time_array[1024];   //timebinarray
+float   waveform[8][1024];  //8x1024 array (8 channels x 1024 samples)
+int     numEvents=1000;     //number of events to record
+int     waveDepth=1024;     //waveform depth of the evaluation board
+double  center=0.0;         //zero point
+double  triglevel=-0.02;    //trigger level (in VOLTS)
+bool    chnOn[4]={};        //channel-On bitmask
+int     trigsource=0;       //choose a trigger source
+double  freq=5;             //sampling frequency
 
 /*------------------------------------------------------------------*/
 
 int main( int argc, char *argv[] )
 {
-    //calibrate board
-    if (strcmp(argv[1],"calibrate")==0)
-        calibrate();
-
-    //filename = argument1 
+    //Take in filename from argv
     const char* filename;
     filename=argv[1];
     if (argc != 2) {
@@ -74,18 +83,18 @@ int main( int argc, char *argv[] )
 
     int i, j, nBoards;
 
-    /* do initial scan */
+    // do initial scan for boards
     drs = new DRS();
 
-    /* show any found board(s) */
+    // show any found board(s)
     for (i=0 ; i<drs->GetNumberOfBoards() ; i++) {
-        b = drs->GetBoard(i);
+        board = drs->GetBoard(i);
         printf("Found DRS4 evaluation board, \
                 serial #%d, firmware revision %d\n", 
-                b->GetBoardSerialNumber(), b->GetFirmwareVersion());
+                board->GetBoardSerialNumber(), board->GetFirmwareVersion());
     }
 
-    /* exit if no board found */
+    // exit if no board found
     nBoards = drs->GetNumberOfBoards();
     if (nBoards == 0) {
         printf("No DRS4 evaluation board found\n");
@@ -95,72 +104,82 @@ int main( int argc, char *argv[] )
     //initialize board
     init();
 
-
-    //parse XML config file for options
+    //parse XML config file for run options
     ParseOptions(); 
-
 
     //print options for current run
     cout << "Running with options: \n";
-    cout << "Trigger Level:"        << triglevel    << "\n";
-    cout << "Trigger Source:"       << trigsource   << "\n";
-    cout << "Zero-point:"           << center       << "\n";
-    cout << "Number of events:"     << numEvents    << "\n";
-    cout << "Sampling frequency:"   << freq <<"GHz" << "\n";
-    cout << "Channel 0 on/off:"     << chnOn[0]     << "\n";
-    cout << "Channel 1 on/off :"    << chnOn[1]     << "\n";
-    cout << "Channel 2 on/off :"    << chnOn[2]     << "\n";
-    cout << "Channel 3 on/off :"    << chnOn[3]     << "\n";
+    cout << "Trigger Level:"        << triglevel        << "\n";
+    cout << "Trigger Source:"       << trigsource       << "\n";
+    cout << "Zero-point:"           << center           << "\n";
+    cout << "Number of events:"     << numEvents        << "\n";
+    cout << "Sampling frequency:"   << freq << "GHz"    << "\n";
+    cout << "Channel 0 on/off:"     << chnOn[0]         << "\n";
+    cout << "Channel 1 on/off :"    << chnOn[1]         << "\n";
+    cout << "Channel 2 on/off :"    << chnOn[2]         << "\n";
+    cout << "Channel 3 on/off :"    << chnOn[3]         << "\n";
 
-    //use following lines to enable hardware trigger 
-    b->EnableTrigger(1, 0);             // enable hardware trigger
-    b->SetTriggerSource(7);
-    b->SetTriggerLevel(-0.05, false);  // trig level, edge
-    b->SetTriggerDelayNs(0);          // trigger delay
+    //trigger options
+    board->EnableTrigger(1, 0);             // enable hardware trigger
+    board->SetTriggerSource(7);             // set trigger source
+    board->SetTriggerLevel(-0.05, false);   // trig level, edge
+    board->SetTriggerDelayNs(0);            // trigger delay
 
     /* NOTE: 
-     * SetTriggerSource accepts an INT
-     * 1 = 1<<0 = 0001 = CHN1
-     * 2 = 1<<1 = 0010 = CHN2
-     * 4 = 1<<2 = 0100 = CHN3
-     * 8 = 1<<3 = 1000 = CHN4
-     * 
-     * 5 = 1<<30101 = CHN3+1
-     * 6 = 0110 = CHN2+3
-     * 7 = 0111 = CHN1+2+3
+     * SetTriggerSource accepts an INT, determined by 12-bit bitmask
      *
-     * etc.
+     * 1<<0 = CHN1 OR
+     * 1<<1 = CHN2 OR
+     * 1<<2 = CHN3 OR
+     * 1<<3 = CHN4 OR
+     * 
+     * 1<<(0+8) = CHN1 AND
+     * 1<<(1+8) = CHN2 AND
+     * 1<<(2+8) = CHN3 AND
+     * 1<<(4+8) = CHN4 AND
+     *
+     * OR and AND triggering is achieved by taking the bitwise OR of 
+     * whatever channels are desired... 
+     *
+     * e.g.:
+     * To trig on CHN1 or CHN2 
+     *      SetTriggerSource(1<<0 | 1<<1)
+     *
+     * To Trig on CHN1 and CHN2 
+     *      SetTriggerSource( 1<<(0+8) | 1<<(1+8) )
+     *
+     * 6 = 0110 = 0100 | 0010        (Trig on CHN2 AND 3)
+     * 7 = 0111 = 0100 | 0010 | 0001 (Trig on CHN1 and 2 and 3)
      *
      */
 
 
-    //open output file
+    // open output file
     int WFfd;
     WFfd = open(filename, O_RDWR | O_CREAT | O_TRUNC | O_BINARY, 0644);
 
-
-    //Collect Events
+    // Collect Events 
     for (int j=0 ; j<numEvents ; j++) {
 
-        /* start board (activate domino wave) */
-        b->StartDomino();
+        // start board (activate domino wave)
+        board->StartDomino();
 
-        /* wait for trigger */
-        while (b->IsBusy());
+        // wait for trigger
+        while (board->IsBusy());
 
-        //Save Waveforms
+        // Save Waveforms
         evSerial=j;
         SaveWaveforms(WFfd); 
 
-        //Print some status
-        //if (j % 100 == 0)
+        // Print some status progress
+        if (j % 100 == 0)
             printf("\nEvent #%d read successfully\n", j);
     }
 
     //Close Waveform File
     close(WFfd);
 
-    /* delete DRS object -> close USB connection */
+    // delete DRS object, close USB connection
     delete drs;
 }
 
@@ -214,10 +233,10 @@ int SaveWaveforms(int fd)
     p += sizeof(unsigned short);
     *(unsigned short *)p = evTimestamp.Milliseconds;
     p += sizeof(unsigned short);
-    *(unsigned short *)p = 0; // reserved
+    *(unsigned short *)p = 0; // reserved bit
     p += sizeof(unsigned short);
 
-    b->GetTime(0, b->GetTriggerCell(0), time_array);
+    board->GetTime(0, board->GetTriggerCell(0), time_array);
 
     for (int j=0 ; j<waveDepth ; j++) {
         // save binary time as 32-bit float value
@@ -225,7 +244,6 @@ int SaveWaveforms(int fd)
             t = (time_array[j]+time_array[j+1])/2;
             j++;
         } 
-
         else
             t = time_array[j];
 
@@ -233,10 +251,13 @@ int SaveWaveforms(int fd)
         p += sizeof(float);
     }
 
-    b->TransferWaves(0, 8);
+    // transfer wave 
+    board->TransferWaves(0, 8);
+    
+    // loop over active channels
     for (int i=0 ; i<4 ; i++) {
         if (chnOn[i]) {
-            b->GetWave(0, i*2, waveform[i]);
+            board->GetWave(0, i*2, waveform[i]);
             sprintf((char *)p, "C%03d", i+1);
             p += 4;
 
@@ -256,9 +277,9 @@ int SaveWaveforms(int fd)
                     *(unsigned short *)p = d;
                     p += sizeof(unsigned short);
                 }
-            }
-        }
-    }
+            } // close for j<waveDepth
+        } // close if chnOn[i]
+    } // close for i<4
 
     int size = p-buffer;
     int n = write(fd, buffer, size);
@@ -268,6 +289,9 @@ int SaveWaveforms(int fd)
     return 1;
 }
 
+//------------------------------------------------------------------------------
+// Parses XML configuration file
+//------------------------------------------------------------------------------
 void ParseOptions(void) //Reads XML config file config.xml
 {
     xml_document<> doc; 
@@ -281,7 +305,7 @@ void ParseOptions(void) //Reads XML config file config.xml
     doc.parse<0>(&buffer[0]);
 
     // Find our root node
-    root_node = doc.first_node("DRS4Config");
+    root_node = doc.first_node("DRS4");
 
     for (xml_node<> * RunConfig = root_node->first_node("RunConfig"); RunConfig; RunConfig = RunConfig->next_sibling()) {
         triglevel=atof(RunConfig->first_attribute("triglevel")->value());
@@ -289,7 +313,6 @@ void ParseOptions(void) //Reads XML config file config.xml
         center=atoi(RunConfig->first_attribute("center")->value());
         numEvents=atoi(RunConfig->first_attribute("numEvents")->value());
         freq=atof(RunConfig->first_attribute("freq")->value());
-        posneg=atoi(RunConfig->first_attribute("posneg")->value());
         chnOn[0]=atoi(RunConfig->first_attribute("chnOn0")->value());
         chnOn[1]=atoi(RunConfig->first_attribute("chnOn1")->value());
         chnOn[2]=atoi(RunConfig->first_attribute("chnOn2")->value());
@@ -297,74 +320,20 @@ void ParseOptions(void) //Reads XML config file config.xml
     }
 }
 
-
-int calibrate()
-{
-    ProgressBar p;
-
-    char line[80];
-    float freq,range;
-
-    //Initialize DRS4 board
-    init();
-
-    printf("\nEnter calibration frequency [GHz]: ");
-    fgets(line, sizeof(line), stdin);
-    freq = atof(line);
-    printf("freq: %f\n",freq);
-    b->SetFrequency(freq, true);
-    cout << "\tVoltage calibration at "<<freq<< " GHz\n";
-
-    printf("Enter center of voltage range [V]: ");
-    fgets(line, sizeof(line), stdin);
-    range = atof(line);
-    b->SetInputRange(range);
-    cout <<"\tRange: "<<center-0.5<<" V - "<<center+0.5<<"V\n";
-
-    //printf("Enter mode [1]024 or [2]048 bin mode: ");
-    //fgets(line, sizeof(line), stdin);
-    //int cascading;
-    //cascading = atoi(line);
-    //if (cascading == 2)
-    b->SetChannelConfig(0, 8, 4);
-    //else
-    //	b->SetChannelConfig(0, 8, 8);
-
-    printf("\nPlease make sure that no input signal are present then hit any key\r");
-    fflush(stdout);
-    while (!kbhit());
-    printf("                                                                  \r");
-    while (kbhit())
-        getchar();
-
-    if (b->GetTransport() == TR_VME)
-        printf("Creating Calibration of Board in VME slot %2d %s, serial #%04d\n",  
-                (b->GetSlotNumber() >> 1)+2, ((b->GetSlotNumber() & 1) == 0) ? "upper" : "lower", 
-                b->GetBoardSerialNumber());
-    else
-        printf("Creating Calibration of Board on USB, serial #%04d\n",  
-                b->GetBoardSerialNumber());
-
-    b->SoftTrigger();
-    b->CalibrateVolt(&p);
-    printf("\nDone! \n");
-
-    cout <<"Calibration finished\n";
-    return 0;
-}
-
-
+//------------------------------------------------------------------------------
+// Initializes DRS board
+//------------------------------------------------------------------------------
 int init()
 {
-    /* continue working with first board only */
-    b = drs->GetBoard(0);
-    /* initialize board */
-    b->Init();
-    /* enable transparent mode needed for analog trigger */
-    b->SetTranspMode(1);
-    /* set input range to -0.5V ... +0.5V - default*/
-    b->SetInputRange(center);
-    /* use following line to set range to 0..1V */
-    //b->SetInputRange(0.5);
+    // continue working with first board only
+    board = drs->GetBoard(0);
+    // initialize board
+    board->Init();
+    // enable transparent mode needed for analog trigger
+    board->SetTranspMode(1);
+    // set input range to -0.5V ... +0.5V - default
+    board->SetInputRange(center);
+    // use following line to set range to 0..1V
+    //board->SetInputRange(0.5);
     return 1;
 }
